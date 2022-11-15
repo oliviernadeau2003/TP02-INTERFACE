aae8ae80fc79f0aa5a7f01e44f9230b6e7949fcd
oliviernadeau2003 <2148279@cstj.qc.ca>
2022-11-15 10:53:25 -0500

    Début Projet

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Wall_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window WallWindow = new WallWindow();
+            WallWindow.Show();
+        }
+
+        private void Marketplace_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window MarketplaceWindow = new MarketplaceWindow();
+            MarketplaceWindow.Show();
+        }
+    }
+}
