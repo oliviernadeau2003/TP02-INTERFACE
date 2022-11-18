1e2f1c9517d06e04015bb91a0d409cdac87510b4
vincent lavoie whitlock <1966857@cstj.qc.ca>
2022-11-18 13:48:25 -0500

    début de l'implémentation des donnés

--- classes/Car.cs
+++ classes/Car.cs
@@ -3,8 +3,9 @@
     public class Car : Offer
     {
         public string Fabricants { get; set; }
+        public int Price { get; set; }
         public string Marque { get; set; }
-        public string Annee { get; set; }
-        public int odometre { get; set; }
+        public int Annee { get; set; }
+        public int Odometre { get; set; }
     }
 }
--- classes/Offer.cs
+++ classes/Offer.cs
@@ -4,7 +4,7 @@ namespace TP2.classes
 {
     public class Offer
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Titre { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
